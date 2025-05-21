0fab29b03ba78bdeeacd7b1b3720607f5e284efc
muditrohilla <rohillamudit679@gmail.com>
2025-05-21 23:36:18 +0530

    UPDATED SOME IMAGES IN EDUCATION

--- components/Education.tsx
+++ components/Education.tsx
@@ -21,14 +21,14 @@ export function Education() {
                     </p>
                     <div className="grid grid-cols-2 gap-4">
                         <Image
-                            src="https://assets.aceternity.com/templates/startup-1.webp"
+                            src="/C_1.jpg"
                             alt="startup template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <Image
-                            src="https://assets.aceternity.com/templates/startup-2.webp"
+                            src="/C_2.jpg"
                             alt="startup template"
                             width={500}
                             height={500}
@@ -53,14 +53,14 @@ export function Education() {
                     </p>
                     <div className="grid grid-cols-2 gap-4">
                         <Image
-                            src="https://assets.aceternity.com/pro/hero-sections.png"
+                            src="/XII_1.jpg"
                             alt="hero template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <Image
-                            src="https://assets.aceternity.com/features-section.png"
+                            src="/xII_2.jpg"
                             alt="feature template"
                             width={500}
                             height={500}
@@ -82,14 +82,14 @@ export function Education() {
                     </p>
                     <div className="grid grid-cols-2 gap-4">
                         <Image
-                            src="https://assets.aceternity.com/pro/hero-sections.png"
+                            src="/X_1.jpg"
                             alt="hero template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
                         />
                         <Image
-                            src="https://assets.aceternity.com/features-section.png"
+                            src="/X_2.jpg"
                             alt="feature template"
                             width={500}
                             height={500}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -60,7 +60,7 @@ const RecentProjects = () => {
                 A small selection of{" "}
                 <SplitText
                     text="Recent Projects!"
-                    className="text-2xl font-semibold text-emerald-500 italic"
+                    className="heading font-semibold text-emerald-500 italic"
                     delay={200}
                     animationFrom={{ opacity: 0, transform: "translate3d(0,40px,0)" }}
                     animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
